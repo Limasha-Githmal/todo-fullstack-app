92affb47ef19f6851c775b91c135d212fadf8137
Limasha-Githmal <limashagithmal@gmail.com>
2026-04-28 11:55:41 +0530

    update server/src/app.js

--- server/src/app.js
+++ server/src/app.js
@@ -9,11 +9,20 @@ const allowedOrigins = [
   'https://todo-fullstack-app.vercel.app', // replace with your actual Vercel URL
 ];
 
-app.use(cors());
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
+
 app.use(express.json());
 
 app.use('/api/todos', todoRoutes);
 
-app.get('/', (req, res) => res.send('API is running ...'));
+app.get('/', (req, res) => res.send('API is running ✅'));
 
 module.exports = app;
